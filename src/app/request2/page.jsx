c7b43014aeb592a79648cbe3d6e69c6253a78a98
Vaibhav262610 <branda56235@gmail.com>
2024-11-24 11:58:44 +0530

    h

--- src/app/request2/page.jsx
+++ src/app/request2/page.jsx
@@ -8,12 +8,12 @@ const page = () => {
         <h1 className='w-80 text-center'>and I also know that you were having mood swings that time but trust me Aditi i really never experience this before because I only approch those to whom i really cared for like you and that was the best time I had in my life</h1>
         <h2 className='text-2xl mt-24 font-semibold text-center'>And I want you in MY LIFE FOREVER <br/> not like a gf but as a FRIEND 💖</h2>
         <h1 className='mt-24'>So, will you please come again in my life like before ?😭</h1>
-        <div className='flex gap-20'>
+        <div className='flex gap-20 flex-col'>
         <Link href='/yayy'>
         <button className="text-2xl font-semibold border-2 hover:bg-black hover:text-white border-black py-2 px-4 text-gray-600">YES</button>
         </Link>
-        <p>There is not a option like NO 😤</p>
         </div>
+        <p>There is not a option like NO 😤</p>
     </div>
   )
 }
