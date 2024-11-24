b7787e55b703615be09c6b54c000064452ffdbf5
Vaibhav262610 <branda56235@gmail.com>
2024-11-24 13:05:35 +0530

    h

--- src/app/confirm/page.jsx
+++ src/app/confirm/page.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='h-screen w-full flex-col gap-8 justify-center items-center flex'>
+        <h1 className='text-9xl element mb-24'>👨‍💻</h1>
       <h1 className='text-center'>So, I going to ask some questions please answer them honestly no lies no excuses</h1>
       <h2 className='text-center'>and yes I never judged you for real....abhi kuch dino mai bht kuch ho gya h dengue, paper chut gya mera and mamy more so just come back bht si baatein krni h 🛐</h2>
       <h2>Mujhe jitna sunana hai suna lena but just come back</h2>
--- src/app/yayy/page.jsx
+++ src/app/yayy/page.jsx
@@ -12,19 +12,20 @@ const page = () => {
   method="POST"
 >
   <label className='text-center text-xl justify-center items-center flex flex-col gap-3 '>
-    Did you forgive me  ????? 😭 (please say yes)
-    <input name="message" className='p-2 outline-none text-sm bg-transparent  w-[370px] border border-black text-black' placeholder='Enter your answer here..' required/>
-  </label>
-  <label className='text-center text-xl justify-center items-center flex flex-col gap-3 '>
-    Will you come again in my life ? 🛐
+    Will you plz plz come again in my life ? 🛐
     <input name="message" className='p-2 text-sm bg-transparent outline-none w-[370px] border border-black text-black' placeholder='Enter your answer here..' required/>
   </label>
+  <label className='text-center text-xl justify-center items-center flex flex-col gap-3 '>
+    Can you forgive me  ????? 😭 (please say yes)
+    <input name="message" className='p-2 outline-none text-sm bg-transparent  w-[370px] border border-black text-black' placeholder='Enter your answer here..' required/>
+  </label>
   <label className='text-xl justify-center items-center text-center flex flex-col gap-3 '>
-    If you wanna send a msg here u can otherwise DM me 😭🙏
+    If you wanna send a msg here u can otherwise DM me ( GAALI BHI DE SKTI HO ) 😭🙏
     <textarea className='p-2 text-sm bg-transparent outline-none w-[370px] border border-black text-black' placeholder='Enter your answer here..' name="message" required></textarea>
   </label>
   <button type='submit' className="mt-24 w-fit text-2xl font-semibold border-2 hover:bg-black hover:text-white border-black py-2 px-4 text-gray-600">SEND</button>
 </form>
+<h2 className='mt-5'>-by AAPKA GUNEGAAR VAIBHAV 🙂</h2>
     </div>  
   )
 }
