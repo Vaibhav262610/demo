8c5b3bf65cb342f51696afc499b48ff35e4bf8f6
Vaibhav262610 <branda56235@gmail.com>
2024-11-24 11:38:08 +0530

    h

--- src/app/gif/page.jsx
+++ src/app/gif/page.jsx
@@ -5,7 +5,7 @@ const page = () => {
     <div className='h-screen w-full justify-center flex-col items-center flex'>
         The things I have said for that
         <h1 className='text-2xl font-semibold'>I&apos;M REALLY REALLY SORRY</h1>
-        <video src="src/public/gif.mp4" controls autoPlay></video>
+        <iframe width="300" height="250" src="https://www.youtube.com/embed/RDApujgI1dM?si=zaTexO7aPW8-bIHE&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   )
 }
