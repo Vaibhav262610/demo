7d784473b3ba3e93ba91e689f44bba438d59219f
Vaibhav262610 <branda56235@gmail.com>
2024-11-24 11:38:58 +0530

    h

--- src/app/gif/page.jsx
+++ src/app/gif/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className='h-screen w-full justify-center flex-col items-center flex'>
         The things I have said for that
-        <h1 className='text-2xl font-semibold'>I&apos;M REALLY REALLY SORRY</h1>
+        <h1 className='text-2xl mb-32 font-semibold'>I&apos;M REALLY REALLY SORRY 🙏</h1>
         <iframe width="300" height="250" src="https://www.youtube.com/embed/RDApujgI1dM?si=zaTexO7aPW8-bIHE&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   )
