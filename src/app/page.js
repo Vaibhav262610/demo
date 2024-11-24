f7c1a68d47e3df739f7cddeb20e926fe005054c6
Vaibhav262610 <branda56235@gmail.com>
2024-11-24 10:43:41 +0530

    h

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
     <>
       <div className=" flex-col h-screen w-full gap-4 flex justify-center items-center">
         <h1 className="text-5xl pb-14">Heyyyyyy 😭</h1>
-        <h2 className="">first of all I deeply apologize</h2>
-        <h2>I know I'm stupid</h2>
-        <h2>but the fact is I really missed you everyday</h2>
+        <h2 className="">first of all I deeply APPOLOGIZE</h2>
+        <h2 className="font-semibold">I know I&apos;m stupid</h2>
+        <h2>but the fact is I really MISSED you everyday</h2>
         <h2 className="mt-24">scroll down</h2>
       </div>
 
